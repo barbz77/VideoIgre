82f3c1ba41e215b31af9b5c9ef3dab58330f241a
Barbara Abramić <abramic7barbara7@gmail.com>
2025-07-01 19:01:10 +0200

    Postavljanje rada s bazom

---
+++ BACKEND/Data/EdunovaContext.cs
@@ -0,0 +1,15 @@
+﻿using BACKEND.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace BACKEND.Data
+{
+    public class EdunovaContext : DbContext
+    {
+        public EdunovaContext(DbContextOptions<EdunovaContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Igrica> Igrice {  get; set; }
+    }
+}
