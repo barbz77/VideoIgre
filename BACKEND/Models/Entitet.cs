b0fee4e338089a964efc49658a81bbb56d4939dd
Barbara Abramić <abramic7barbara7@gmail.com>
2025-07-01 18:23:07 +0200

    Model klase

---
+++ BACKEND/Models/Entitet.cs
@@ -0,0 +1,7 @@
+﻿namespace BACKEND.Models
+{
+    public abstract class Entitet
+    {
+        public int Sifra {  get; set; }
+    }
+}
---
+++ BACKEND/Models/Igrice.cs
@@ -0,0 +1,9 @@
+﻿namespace BACKEND.Models
+{
+    public class Igrica : Entitet
+    {
+        public string Naziv { get; set; } = "";
+        public decimal Ocjena { get; set; }
+        public DateTime GodinjaIzdanja { get; set; }
+    }
+}
